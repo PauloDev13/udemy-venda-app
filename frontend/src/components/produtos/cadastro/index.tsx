98d994a5a7d6f772124ee5ce1ec7aa0659132449
PauloDev13 <devpgm2020@gmail.com>
2022-01-03 21:56:36 -0300

    Creating Product Form

--- frontend/src/components/produtos/cadastro/index.tsx
+++ frontend/src/components/produtos/cadastro/index.tsx
@@ -4,6 +4,63 @@ import { Layout } from '~/components/layout';
 
 export const CadastroProduto: NextPage = () => {
   return (
-    <Layout title="Cadastro de Produtos">Aqui vai ficar o formulário</Layout>
+    <Layout title="Cadastro de Produtos">
+      <div className="field">
+        <label className="label" htmlFor="inputSku">
+          SKU *
+        </label>
+        <div className="control">
+          <input
+            className="input"
+            id="inputSku"
+            placeholder="Digite o SKU do produto"
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label" htmlFor="inputPrice">
+          Preço *
+        </label>
+        <div className="control">
+          <input
+            className="input"
+            id="inputPrice"
+            placeholder="Digite o preço do produto"
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label" htmlFor="inputName">
+          Nome *
+        </label>
+        <div className="control">
+          <input
+            className="input"
+            id="inputName"
+            placeholder="Digite o nome do produto"
+          />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label" htmlFor="inputDescricao">
+          Descrição
+        </label>
+        <div className="control">
+          <textarea
+            className="textarea"
+            id="inputDescricao"
+            placeholder="Digite uma descrição detalhada do produto"
+          />
+        </div>
+      </div>
+      <div className="field is-grouped">
+        <div className="control">
+          <button className="button is-link">Salvar</button>
+        </div>
+        <div className="control">
+          <button className="button is-link is-light">Cancelar</button>
+        </div>
+      </div>
+    </Layout>
   );
 };
