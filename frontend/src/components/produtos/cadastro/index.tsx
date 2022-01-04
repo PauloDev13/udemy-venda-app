3d3ea80b3f44111a3a40ae407348f03bf2b31ef1
PauloDev13 <devpgm2020@gmail.com>
2022-01-04 00:19:24 -0300

    Manipulating Forms Data with useState

--- frontend/src/components/produtos/cadastro/index.tsx
+++ frontend/src/components/produtos/cadastro/index.tsx
@@ -1,8 +1,26 @@
+import { useState } from 'react';
+
 import { NextPage } from 'next';
 
 import { Layout } from '~/components/layout';
 
 export const CadastroProduto: NextPage = () => {
+  const [sku, setSku] = useState('');
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
+
+  const submit = () => {
+    const produto = {
+      sku,
+      price,
+      name,
+      description,
+    };
+    // eslint-disable-next-line no-console
+    console.log(produto);
+  };
+
   return (
     <Layout title="Cadastro de Produtos">
       <div className="columns">
@@ -12,6 +30,8 @@ export const CadastroProduto: NextPage = () => {
           </label>
           <div className="control">
             <input
+              value={sku}
+              onChange={(event) => setSku(event.target.value)}
               className="input"
               id="inputSku"
               placeholder="Digite o SKU do produto"
@@ -24,6 +44,8 @@ export const CadastroProduto: NextPage = () => {
           </label>
           <div className="control">
             <input
+              value={price}
+              onChange={(event) => setPrice(event.target.value)}
               className="input"
               id="inputPrice"
               placeholder="Digite o preço do produto"
@@ -38,6 +60,8 @@ export const CadastroProduto: NextPage = () => {
           </label>
           <div className="control">
             <input
+              value={name}
+              onChange={(event) => setName(event.target.value)}
               className="input"
               id="inputName"
               placeholder="Digite o nome do produto"
@@ -52,6 +76,8 @@ export const CadastroProduto: NextPage = () => {
           </label>
           <div className="control">
             <textarea
+              value={description}
+              onChange={(event) => setDescription(event.target.value)}
               className="textarea"
               id="inputDescricao"
               placeholder="Digite uma descrição detalhada do produto"
@@ -61,7 +87,9 @@ export const CadastroProduto: NextPage = () => {
       </div>
       <div className="field is-grouped">
         <div className="control">
-          <button className="button is-link">Salvar</button>
+          <button onClick={submit} className="button is-link">
+            Salvar
+          </button>
         </div>
         <div className="control">
           <button className="button is-link is-light">Cancelar</button>
