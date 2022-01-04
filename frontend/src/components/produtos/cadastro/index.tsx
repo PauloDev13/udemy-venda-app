fc6a76d2606ceb184e2c091bda998bdd127d23ce
PauloDev13 <devpgm2020@gmail.com>
2022-01-03 22:24:22 -0300

    Aligning product form fields

--- frontend/src/components/produtos/cadastro/index.tsx
+++ frontend/src/components/produtos/cadastro/index.tsx
@@ -5,52 +5,58 @@ import { Layout } from '~/components/layout';
 export const CadastroProduto: NextPage = () => {
   return (
     <Layout title="Cadastro de Produtos">
-      <div className="field">
-        <label className="label" htmlFor="inputSku">
-          SKU *
-        </label>
-        <div className="control">
-          <input
-            className="input"
-            id="inputSku"
-            placeholder="Digite o SKU do produto"
-          />
+      <div className="columns">
+        <div className="field is-half column">
+          <label className="label" htmlFor="inputSku">
+            SKU *
+          </label>
+          <div className="control">
+            <input
+              className="input"
+              id="inputSku"
+              placeholder="Digite o SKU do produto"
+            />
+          </div>
+        </div>
+        <div className="field is-half column">
+          <label className="label" htmlFor="inputPrice">
+            Preço *
+          </label>
+          <div className="control">
+            <input
+              className="input"
+              id="inputPrice"
+              placeholder="Digite o preço do produto"
+            />
+          </div>
         </div>
       </div>
-      <div className="field">
-        <label className="label" htmlFor="inputPrice">
-          Preço *
-        </label>
-        <div className="control">
-          <input
-            className="input"
-            id="inputPrice"
-            placeholder="Digite o preço do produto"
-          />
+      <div className="columns">
+        <div className="field is-full column">
+          <label className="label" htmlFor="inputName">
+            Nome *
+          </label>
+          <div className="control">
+            <input
+              className="input"
+              id="inputName"
+              placeholder="Digite o nome do produto"
+            />
+          </div>
         </div>
       </div>
-      <div className="field">
-        <label className="label" htmlFor="inputName">
-          Nome *
-        </label>
-        <div className="control">
-          <input
-            className="input"
-            id="inputName"
-            placeholder="Digite o nome do produto"
-          />
-        </div>
-      </div>
-      <div className="field">
-        <label className="label" htmlFor="inputDescricao">
-          Descrição
-        </label>
-        <div className="control">
-          <textarea
-            className="textarea"
-            id="inputDescricao"
-            placeholder="Digite uma descrição detalhada do produto"
-          />
+      <div className="columns">
+        <div className="field is-full column">
+          <label className="label" htmlFor="inputDescricao">
+            Descrição
+          </label>
+          <div className="control">
+            <textarea
+              className="textarea"
+              id="inputDescricao"
+              placeholder="Digite uma descrição detalhada do produto"
+            />
+          </div>
         </div>
       </div>
       <div className="field is-grouped">
