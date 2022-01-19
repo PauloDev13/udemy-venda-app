96542b7baf64af5423636d11c48285e9c860b506
PauloDev13 <devpgm2020@gmail.com>
2022-01-19 15:32:52 -0300

    Frontend - Adding Primereact library

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app';
 import '~/components/common/loader/loader.css';
 import 'bulma/css/bulma.css';
+import 'primereact/resources/primereact.min.css';
+import 'primereact/resources/themes/luna-blue/theme.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
