ca1246de4674a7bdabae79489ea83b79d6c13d41
PauloDev13 <devpgm2020@gmail.com>
2022-01-20 19:21:53 -0300

    Frontend - Adding product selection fields

--- frontend/src/components/vendas/cadastro/FormVenda.tsx
+++ frontend/src/components/vendas/cadastro/FormVenda.tsx
@@ -8,6 +8,7 @@ import {
   AutoCompleteCompleteMethodParams,
 } from 'primereact/autocomplete';
 import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
 
 import { ClienteModel } from '~/app/model/clienteModel';
 import { Page } from '~/app/model/Page';
@@ -28,6 +29,7 @@ const formSchema: VendaModel = {
 export const FormVenda: NextPage<VendasFormProps> = ({ onSubmit }) => {
   const service = useClienteService();
 
+  const [codigoProduto, setCodigoProduto] = useState<string>('');
   const [clientes, setClientes] = useState<Page<ClienteModel>>({
     content: [],
     page: 0,
@@ -48,6 +50,10 @@ export const FormVenda: NextPage<VendasFormProps> = ({ onSubmit }) => {
     formik.setFieldValue('cliente', clienteSelecionado);
   };
 
+  const handleCodigoProdutoSelect = () => {
+    console.log(codigoProduto);
+  };
+
   const formik = useFormik({
     onSubmit,
     initialValues: formSchema,
@@ -55,17 +61,46 @@ export const FormVenda: NextPage<VendasFormProps> = ({ onSubmit }) => {
   return (
     <form onSubmit={formik.handleSubmit}>
       <div className="p-fluid">
-        <div className="field col-12">
-          <label htmlFor="cliente">Cliente: *</label>
-          <AutoComplete
-            id="cliente"
-            name="cliente"
-            field="name"
-            value={formik.values.cliente}
-            suggestions={clientes.content}
-            completeMethod={handleClienteAutocomplete}
-            onChange={handleClienteChange}
-          />
+        <div className="grid">
+          <div className="field col-12">
+            <label htmlFor="cliente">Cliente: *</label>
+            <AutoComplete
+              className="p-inputtext-sm"
+              id="cliente"
+              name="cliente"
+              field="name"
+              value={formik.values.cliente}
+              suggestions={clientes.content}
+              completeMethod={handleClienteAutocomplete}
+              onChange={handleClienteChange}
+            />
+          </div>
+        </div>
+        <div className="grid">
+          <div className="field col-12 md:col-2">
+            <span className="p-float-label">
+              <InputText
+                className="p-inputtext-sm"
+                id="codigo"
+                value={codigoProduto}
+                onBlur={handleCodigoProdutoSelect}
+                onChange={(e) => setCodigoProduto(e.target.value)}
+              />
+              <label htmlFor="codigo">Código</label>
+            </span>
+          </div>
+          <div className="field md:col-6">
+            <AutoComplete className="p-inputtext-sm" />
+          </div>
+          <div className="field col-12 md:col-2">
+            <span className="p-float-label">
+              <InputText id="codigo" className="p-inputtext-sm" />
+              <label htmlFor="codigo">QTD</label>
+            </span>
+          </div>
+          <div className="field col-12 md:col-2">
+            <Button label="Adicionar" className="p-button" />
+          </div>
         </div>
         <Button label="Finalizar" type="submit" />
       </div>
